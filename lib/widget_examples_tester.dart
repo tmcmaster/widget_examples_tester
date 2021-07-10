86ae2060c89cceddd3cc124fe4b7044c89af4597
Tim McMaster <tim@mcmaster.id.au>
2021-07-10 10:54:08 +1000

    added export for widget_examples_tester_providers

--- lib/widget_examples_tester.dart
+++ lib/widget_examples_tester.dart
@@ -3,3 +3,4 @@ library widget_examples_tester;
 export 'src/examples_builder.dart';
 export 'src/styled_riverpod_app.dart';
 export 'src/widget_examples_tester.dart';
+export 'src/widget_examples_tester_providers.dart';
